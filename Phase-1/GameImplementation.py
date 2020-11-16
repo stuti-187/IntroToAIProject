28b80917a9c882d9790ed89b21711c6c05e4b7e2
stuti-187 <stuti.1996@gmail.com>
2020-11-15 21:51:05 -0500

    Update GameImplementation.py

--- Phase-1/GameImplementation.py
+++ Phase-1/GameImplementation.py
@@ -1,7 +1,6 @@
 import numpy as np
 import random
 
-depth = 9
 #Initialize the game board
 def createMatrix(n):
     return np.zeros(shape=(n, n))
@@ -54,8 +53,6 @@ def markVisited(n,g,s,t,player, actions):
 
 #checks if a triangle is formed by a player
 def checkTriangle(n, g, s, t, player):
-    if(s==None or t==None):
-        return False
     for i in range(n):
         if(g[i,s] == player and g[i,t] == player):
             return True
@@ -87,119 +84,10 @@ def getInput(g, player):
         getInput(g, player)
     return (player_move_s, player_move_t)
 
-
-#fetch the count of edges in graph g of "player" connected to "vertex"
-def degree(n, g, vertex, player):
-    count = 0
-    for i in range(n):
-        if(g[i, vertex]==player):
-            count += 1
-    return (count)
-
-#scoring method for minimax evaluation
-#human player is expected to decrease the score for AI and AI will want to choose best possible move
-def minmaxEvaluation(n,g):
-    i = 0
-    for j in range(n):
-        if(degree(n, g, j, 1) == 1):
-            i+=4
-        #higher the degree for human better the score for AI
-        if(degree(n, g, j, 1) > 1):
-            i+=7
-        if(degree(n, g, j, 2) == 1):
-            i-=4
-        #higher the degree for AI lower the score for AI
-        if(degree(n, g, j, 2) > 1):
-            i-=7
-    return i
-
-
-def maxAlphaBeta(n, g,countVisitedEdges, actions,d):
-    maxv = -10000000
-    s = None
-    t = None
-    completeGraph = (n*(n-1))/2
-    if(depth==d or completeGraph < countVisitedEdges):
-        return(minmaxEvaluation(n,g),0,0)
-    #     return (0, 0, 0)
-    elif(checkTriangle(n, g, s, t, 2)):
-        return (-1, 0, 0)
-    elif(checkTriangle(n, g, s, t, 1)):
-        return (1, 0, 0)
-    # elif(depth==d):
-        # return(minmaxEvaluation(n,g),s,t)
-
-    for i in range(n):
-        for j in range(i+1,n):
-            if (i,j) in actions:
-                g[i, j] = 2
-                g[j, i] = 2
-                actions.remove((i, j))
-                actions.remove((j, i))
-                (currScore, minS, minT) = minAlphaBeta(n, g, countVisitedEdges+1, actions, d+1)
-                if(currScore > maxv):
-                    maxv = currScore
-                    s = i
-                    t = j
-                g[i, j] = 0
-                g[j, i] = 0
-                actions.add((i, j))
-                actions.add((j, i))
-
-                # if (maxv >= b):
-                #     return (maxv,s,t)
-                #
-                # if(maxv > a and d%2==0):
-                #     a = maxv
-
-    return(maxv, s, t)
-
-
-def minAlphaBeta(n, g, countVisitedEdges, actions,d):
-    minv = 10000000
-    s = None
-    t = None
-    completeGraph = (n*(n-1))/2
-    if(depth==d or completeGraph < countVisitedEdges):
-        return(minmaxEvaluation(n,g),s,t)
-    #     return (0, 0, 0)
-    elif(checkTriangle(n, g, s, t, 2)):
-        return (-1, 0, 0)
-    elif(checkTriangle(n, g, s, t, 1)):
-        return (1, 0, 0)
-    # elif(depth==d):
-        return(minmaxEvaluation(n,g),s,t)
-
-    for i in range(n):
-        for j in range(i+1, n):
-            if (i,j) in actions:
-                g[i, j] = 1
-                g[j, i] = 1
-                actions.remove((i, j))
-                actions.remove((j, i))
-                (currScore, maxS, maxT) = maxAlphaBeta(n, g,countVisitedEdges+1, actions, d+1)
-                if(minv > currScore):
-                    minv = currScore
-                    s = i
-                    t = j
-                g[i, j] = 0
-                g[j, i] = 0
-                actions.add((i, j))
-                actions.add((j, i))
-
-                # if (minv <= a):
-                #     return (minv,s,t)
-                #
-                # if(minv < b and d%2==1):
-                #     b = minv
-
-    return(minv, s, t)
-
-
 #main
 if __name__ == '__main__':
     print("Welcome to SIM Game!")
-    player = 1    #1: human     2: AI
+    player = 1
     checkWinFlag = False
     #n: stores the input size
     n = int(input("Enter game size: "))
@@ -221,12 +109,7 @@ if __name__ == '__main__':
     while (count <= completeGraph): #Stopping condition: check if number of possible edges exhausted
         #player_move_s: stores the source vertex of the player
         #player_move_t: stores the target vertex of the player
-        if player==1:
-            player_move_s, player_move_t = getInput(g, player)
-        else:
-            (score, player_move_s, player_move_t) = maxAlphaBeta(n, g, count, actions, 0)
-            print(player_move_s, " ", player_move_t)
-
+        player_move_s, player_move_t = getInput(g, player)
 
         #if triangle is formed by a player, game ends and opposition wins
         if (checkTriangle(n, g, player_move_s, player_move_t, player)):
